5037e46a3ff980b9a32b209af8d5535a432151c2
dhaddasa <sanvirsingh@outlook.com>
2021-05-28 18:58:10 +0530

    New branch

---
+++ src/student__app/PartTimeStudent.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+This is only for git demo.
+ */
+package student__app;
+
+/**
+ *
+ * @author Sanvir Singh Dhadda
+ */
+public class PartTimeStudent {
+    
+}
